2755adfd337b87dae994d0bd8c234d84064e3dc5
lotero <lgo3000@gmail.com>
2019-10-30 17:48:33 -0300

    added NASA's data fetching capabilities

--- src/components/NASAGallery/NASAGallery.js
+++ src/components/NASAGallery/NASAGallery.js
@@ -5,13 +5,44 @@ import ImageCarousel from './ImageCarousel/ImageCarousel'
 import './NASAGallery.css'
 
 
-function NASAGallery() {
-    return (
-        <div className="nasa-gallery-container">
-            <Picture />
-            <ImageCarousel />
-        </div>
-    )
+class NASAGallery extends React.Component {
+
+    state = {
+        isLoading: true,
+        data: null
+    }
+
+    fetchNASAData = () => {
+        fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+            .then(res => res.json())
+            .then(json => {
+                console.log(json)
+                
+                this.setState({ 
+                    data: json,
+                    isLoading: false
+                })
+            })
+    }
+
+    componentDidMount() {
+        this.fetchNASAData()
+    }
+
+    render() {
+        if (this.state.isLoading){
+            return (
+                <div>Estamos haciendo la pegada al servidor</div>
+            )
+        }
+
+        return (
+            <div className="nasa-gallery-container">
+                <Picture imgUrl={this.state.data.url} />
+                <ImageCarousel />
+            </div>
+        )
+    }
 }
 
 export default NASAGallery
--- src/components/NASAGallery/Picture.js
+++ src/components/NASAGallery/Picture.js
@@ -1,8 +1,7 @@
 import React from 'react'
 
-
-function Picture(props){
-    return <img src="" alt="imagen no encontrada" />
+const Picture = (props) => {
+    return <img src={props.imgUrl} alt="imagen no encontrada" />
 }
 
 export default Picture
